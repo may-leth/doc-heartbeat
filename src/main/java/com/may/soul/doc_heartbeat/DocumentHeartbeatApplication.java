f67d5b793c8bc96206e105dea17d5e27b1cba6fe
may-leth <mayleth9609@gmail.com>
2026-01-29 10:43:12 +0100

    chore: initial project setup with Spring Boot

---
+++ src/main/java/com/may/soul/doc_heartbeat/DocumentHeartbeatApplication.java
@@ -0,0 +1,13 @@
+package com.may.soul.doc_heartbeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentHeartbeatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentHeartbeatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/may/soul/doc_heartbeat/DocumentHeartbeatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.may.soul.doc_heartbeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentHeartbeatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
